d524bee946b41592c10d471d344fe4c3985df4d0
cleveraziko <azamat770797@gmail.com>
2021-12-20 21:31:59 +0500

    adding model

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def projects(request):
+    msg = 'hello'
+    return render(request, 'projects/project.html', {'msg':msg})
+
+
+def project(request,pk):
+    return render(request, 'projects/single_project.html')
